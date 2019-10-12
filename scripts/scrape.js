ed964a122d07db5cd3eff78f7c1ecb5e5386ea36
LindoK <khavin.lindo@gmail.com>
2019-10-12 12:38:52 -0400

    Scraping is functioning

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -14,11 +14,10 @@ var scrape = function (callback) {
         var articles = [];
 
         $(".css-8atqhb").each(function(i, element) {
-            //var head = $(element).find("a").children(".es182me1").text().trim();
-            //var sum = $(element).find("a").find("e1n8kpyg0").text().trim();
 
-            var head = $(element).text().trim();
-            var sum = $(element).text().trim();
+            var head = $(element).find("h2").text().trim();
+            var sum = $(element).find("p").text().trim();
+            var url =$(element).find("a").attr("href");
 
             if(head && sum) {
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
@@ -26,8 +25,10 @@ var scrape = function (callback) {
 
                 var dataToAdd = {
                     headLine: headNeat,
-                    summary: sumNeat
+                    summary: sumNeat,
+                    link: "https://www.nytimes.com" + url
                 };
+                
                 articles.push(dataToAdd);
             }
         });
